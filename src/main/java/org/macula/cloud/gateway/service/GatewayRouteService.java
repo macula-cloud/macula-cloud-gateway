b2ad015ad9b049276dadc529a566c6644e273df5
Jokeway <jokeway@hotmail.com>
2020-05-19 17:01:15 +0800

    [IMP] update cache

--- src/main/java/org/macula/cloud/gateway/service/GatewayRouteService.java
+++ src/main/java/org/macula/cloud/gateway/service/GatewayRouteService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.macula.cloud.gateway.domain.GatewayRoute;
 import org.macula.cloud.gateway.repository.GatewayRouteRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class GatewayRouteService {
 		this.repository = repository;
 	}
 
+	@Cacheable(cacheNames = "routes", unless = "#result == null || #result.isEmpty()")
 	public List<GatewayRoute> loadGatewayRoutes() {
 		return repository.findAll(Sort.by(Order.asc("ordered")));
 	}
