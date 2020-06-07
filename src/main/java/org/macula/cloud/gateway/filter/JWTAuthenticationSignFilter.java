6dd84ce7456159e51ce69eeb4fe7dc4d8d7ce595
Jokeway <jokeway@hotmail.com>
2020-06-07 15:54:18 +0800

    [FIX] fix jwt filter

--- src/main/java/org/macula/cloud/gateway/filter/JWTAuthenticationSignFilter.java
+++ src/main/java/org/macula/cloud/gateway/filter/JWTAuthenticationSignFilter.java
@@ -5,6 +5,8 @@ import org.macula.cloud.core.utils.SecurityUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.jwt.crypto.sign.Signer;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.web.server.ServerWebExchange;
@@ -15,14 +17,18 @@ import reactor.core.publisher.Mono;
 @AllArgsConstructor
 public class JWTAuthenticationSignFilter implements GlobalFilter {
 
+	private static final SecurityContext EMPTY_CONTEXT = new SecurityContextImpl();
 	private final ServerSecurityContextRepository repository;
 	private Signer jwtSigner;
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		return repository.load(exchange).map(context -> {
-			if (context != null) {
-				SubjectPrincipal principal = SecurityUtils.getLoginedPrincipal(context.getAuthentication());
+		return repository.load(exchange).defaultIfEmpty(EMPTY_CONTEXT).map(context -> {
+			SubjectPrincipal principal = SecurityUtils.getLoginedPrincipal();
+			if (principal == null && context != EMPTY_CONTEXT) {
+				principal = SecurityUtils.getLoginedPrincipal(context.getAuthentication());
+			}
+			if (principal != null) {
 				String jwt = SecurityUtils.convertPrincipal(principal, jwtSigner);
 				if (jwt != null) {
 					ServerHttpRequest request = exchange.getRequest().mutate().header("Authorization", jwt).build();
